700550e4926ac5c44ebe234a1be6b5bd880cf203
Your Name <your_email@example.com>
2024-10-28 01:04:32 +0530

    Initial commit of portfolio website

---
+++ config.js
@@ -0,0 +1,70 @@
+window.PorfolioConfig = {
+  name: 'Shreya Gantayat',
+  jobTitle: 'Cybersecurity & Machine Learning Enthusiast | Content Writer',
+  jobDescription: 'An aspiring ethical hacker exploring computer networks, cybersecurity, and the integration of machine learning in these domains, with a passion for content writing.',
+  resume: [
+    { name: 'Click me',
+      link: 'https://drive.google.com/file/d/17LLIYhdEs3s8oGF8SBtBXbz-FVNTQaR3/view?usp=sharing',
+      description: 'Check out my resume for more information!'
+    }
+  ],
+  skills: [
+    {
+      name: 'Ethical Hacking',
+      description: 'Learning how to find and fix vulnerabilities in systems to help organizations stay secure.'
+    },
+    {
+      name: 'Penetration testing',
+      description: 'Exploring ways to test systems and networks by simulating real-world attacks to identify weak points.'
+    },
+    {
+      name: 'GenAI and Machine Learning',
+      description: 'Understanding how AI/ML models can support cybersecurity tasks like threat detection and analysis.'
+    },
+    {
+      name: 'Network Engineering',
+      description: 'Gaining knowledge in setting up and managing computer networks to ensure smooth and secure communication.'
+    },
+    {
+      name: 'Blockchain security',
+      description: 'Exploring how blockchain technology secures data and prevents tampering through decentralized systems.'
+    }
+  ],
+  experiences: [
+    {
+      name: 'Network Intrusion Detection System',
+      description: 'Developed a network based intrusion detection tool to capture and monitor network traffic and identify potential threats.'
+    },
+    {
+      name: 'SEHAT: A centralised and secure healthcare record maintenance app',
+      link: 'https://sih-front.vercel.app/',
+      description: 'Contributed to designing a secure app for managing patient data using blockchain, biometric authentication, and QR-code systems, gaining hands-on experience with blockchain implementation. The project was selected at the SIH hackathon at national level.'
+    }, 
+    {
+      name: 'Patent for a Secure and Encrypted messaging platform',
+      link: './PatentEncryption.jpg',
+      description: 'Developed the concept for an innovative messaging platform focused on encryption and user privacy, ensuring secure communication.'
+    }
+  ],
+  certifications: [
+    {
+      name: 'Microsoft Azure Fundamentals AI-900',
+      link: './Microsoft Certification_page-0001.jpg',
+      description: 'Microsoft Azure Fundamentals for AI. This certification qualifies me as an AI engineer.'
+    }
+  ],
+  contacts: {
+    email: 'shreyagantayat03@gmail.com',
+    mobileNo: '+91 7606023277',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/gantayat03'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/shreya-gantayat-53b023278'
+      }
+    ]
+  }
+};
